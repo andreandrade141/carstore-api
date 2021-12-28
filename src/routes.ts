e752a87f3cb2252b9a38aceeaf8b13266df37188
André Andrade <andrelsandrade43@gmail.com>
2021-12-28 20:21:05 -0300

    Correction for Missing modules

--- src/routes.ts
+++ src/routes.ts
@@ -1,39 +1,47 @@
 import express from "express";
 import UserController from "./controllers/UserController";
 import VeicController from "./controllers/VeicController";
+import VendaController from "./controllers/VendaController";
 import { IsAuthenticated } from "./middleware/IsAuthenticated";
 import { auth } from "./routes/auth";
 import { secret } from "./routes/secret";
 
 const router = express.Router();
 
-// AUTH
+// AUTHENTICATION
 router.post("/auth", auth);
 // AUTH CONFIRM
-router.get("/secret", IsAuthenticated, secret);
+router.get("/secret", secret);
 
 // USER
-// CREATE
+// CREATE - PRIVADO
 router.post("/users", IsAuthenticated, UserController.create);
-// READ
+// READ - PUBLICO
 router.get("/users", UserController.findAll);
-// READ
+// READ - PUBLICO
 router.get("/users/:userId", UserController.findOne);
-// UPDATE
+// UPDATE - PRIVADO
 router.put("/users/:userId", IsAuthenticated, UserController.update);
-// DELETE
+// DELETE - PRIVADO
 router.delete("/users/:userId", IsAuthenticated, UserController.destroy);
 
 // VEICULOS
-// CREATE
+// CREATE - PRIVADO
 router.post("/veiculos", IsAuthenticated, VeicController.create);
-// READ
+// READ - PUBLICO
 router.get("/veiculos", VeicController.findAll);
-// READ
-router.get("/veiculos/veicId", VeicController.findOne);
-// UPDATE
-router.put("/veiculos/veicId", IsAuthenticated, VeicController.update);
-// DELETE
-router.delete("/veiculos/veicId", IsAuthenticated, VeicController.delete);
+// READ - PUBLICO
+router.get("/veiculos/:veicId", VeicController.findOne);
+// UPDATE - PRIVADO
+router.put("/veiculos/:veicId", IsAuthenticated, VeicController.update);
+// DELETE - PRIVADO
+router.delete("/veiculos/:veicId", IsAuthenticated, VeicController.delete);
+
+// VENDAS
+// READ ALL VENDAS
+router.get("/vendas/:status", IsAuthenticated, VendaController.findRes);
+router.get("/vendas/:status", IsAuthenticated, VendaController.findVend);
+// READ ALL BY idVendedor
+router.get("/vendedor/:id", IsAuthenticated, VendaController.findbyId);
 
 export { router };
