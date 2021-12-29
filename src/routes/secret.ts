4169f73ee0083552b58e9028d62d705c78500187
André Andrade <andrelsandrade43@gmail.com>
2021-12-28 21:08:06 -0300

    Update Branch

--- src/routes/secret.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-
-export const secret = (req: Request, res: Response) => {
-  // eslint-disable-next-line dot-notation
-  res.send(JSON.stringify(req["tokenData"]));
-};
